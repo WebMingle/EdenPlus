e84fe1cfd813c38811a20f2bea9544a2642096b5
Adrian Alvird <51330833+adrianalvird@users.noreply.github.com>
2024-02-25 09:17:23 +0530

    Add files via upload

---
+++ static/account/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#birth-date').mask('00/00/0000');
+  $('#phone-number').mask('0000-0000');
+ })
+ 
+ 
+ 
+ setTimeout(function(){
+    $('#message').fadeOut('slow')
+ }, 4000)
